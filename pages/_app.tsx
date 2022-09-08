8b272ee600c91e875fbfcc3e85c185e0e34ef22a
Guilherme Magno <guilhermertfmagno@gmail.com>
2022-09-08 14:04:59 -0300

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+
+import '../styles/reset.scss'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
