364c414dc1612b95ef92ddb1843d451ba4617cc1
Guilherme Magno <guilhermertfmagno@gmail.com>
2022-09-22 22:01:28 -0300

    docs: initial storybook project

---
+++ .storybook/docTheme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming'
+
+// https://storybook.js.org/docs/react/configure/theming
+export default create({
+  base: 'dark',
+  brandTitle: 'Next.js Pokédex',
+  brandUrl: 'http://next-pokedex-seven.vercel.app/',
+  brandImage: 'http://next-pokedex-seven.vercel.app/small-logo.png',
+  brandTarget: '_blank',
+})
